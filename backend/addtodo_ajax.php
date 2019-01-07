9cb673f3a62039aa9e5136bbd86014decb469988
Jatin Parab <sulphurgfx@gmail.com>
2019-01-08 03:51:20 +0530

    Fix quote todos

--- backend/addtodo_ajax.php
+++ backend/addtodo_ajax.php
@@ -3,6 +3,7 @@
     include 'conn.php';
     $data = $_POST;
     $content = $data['content'];
+    $content = addslashes($content);
     $sql = "INSERT INTO todolist(content) VALUES ('$content')";
     $result = $conn->query($sql);
     if($result){
